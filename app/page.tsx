2c7681e182a45f3410bb1f15681bf655e102c5c2
CoopTRUE <coopdog4545@gmail.com>
2024-05-06 14:02:57 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="container">
+      <p>Hi</p>
+      <Button>Button</Button>
+    </main>
+  )
+}
