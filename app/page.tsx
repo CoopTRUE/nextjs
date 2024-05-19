f4cabbd1af58916b89e1a9e8432f25d8fca19919
CoopTRUE <coopdog4545@gmail.com>
2024-05-19 12:16:05 -0700

    added react compiler :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+'use client'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+  const [count, setCount] = useState(0)
+  const plus1 = count + 1
+  const [count2, setCount2] = useState(0)
+
   return (
     <main className="container">
       <p>Hi</p>
-      <Button>Button</Button>
+      <Button onClick={() => setCount((count) => count + 1)}>
+        Clicked {plus1} time{count === 1 ? '' : 's'}
+      </Button>
     </main>
   )
 }
